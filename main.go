02efbc371892fc52ad3da26d7e4d3b9b4020b834
Dwiki Yulian <79854897+qjvxvL@users.noreply.github.com>
2025-11-29 09:52:04 +0700

    refactor: organize imports and improve user input flow

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName string = "Go Conference"
@@ -15,43 +18,50 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userName string
+		var userTickets uint
+
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName) 
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
+
+		fmt.Printf("The whole array: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Slice type: %T\n", bookings)
+		fmt.Printf("Slice length: %v\n", len(bookings))
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+		userTickets = 2
+		fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		var firstNames []string
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			var firstName = names[0]
+			firstNames = append(firstNames, firstName)
+		}
+
+		fmt.Printf("The first name of bookings are : %v\n", firstNames)
+	}
 	
-
-	var firstName string
-	var lastName string
-	var email string
-	var userName string
-	var userTickets uint
-
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName) 
-
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
-
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
-
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Slice type: %T\n", bookings)
-	fmt.Printf("Slice length: %v\n", len(bookings))
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-	fmt.Printf("These are all our bookings: %v\n", bookings)
-	 
 	// fmt.Println(conferenceName)
 	// fmt.Println(conferenceTickets)
 	
